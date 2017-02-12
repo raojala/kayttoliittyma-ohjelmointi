7de589f51d32812044d012062d42e956ee0c97d7
Rami <Rami@DESKTOP-O8F0SOE>
2017-02-12 16:22:46 +0200

    labra 10 tehtava 4 valmis

---
+++ kayttoliittymaohjelmointi/Labra_10_Tehtava_4/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labra_10_Tehtava_4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        kiuas Kiuas;
+        string s = "";
+        double temp, humi;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void customClick(object sender, RoutedEventArgs e)
+        {
+            s += sender.ToString().Substring(sender.ToString().Length-1, 1);
+            txtResult.Text = s;
+        }
+
+        private void btnAccept_Click(object sender, RoutedEventArgs e)
+        {
+            if (rdbHumidity.IsChecked == true)
+            {
+                float f;
+                float.TryParse(s, out f);
+                Kiuas.SetHumid(f);
+                lblHumidity.Content = Kiuas.Humid;
+
+            }
+            else if (rdbTemperature.IsChecked == true)
+            {
+                float f;
+                float.TryParse(s, out f);
+                Kiuas.SetTemp(f);
+                lblTemperature.Content = Kiuas.Temp;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Kiuas = new kiuas();
+        }
+
+        private void btnBackspace_Click(object sender, RoutedEventArgs e)
+        {
+            // tee tarkastus että pyyhkiminen ei voi mennä negatiiviseks
+            s = txtResult.Text.Substring(0, txtResult.Text.Length - 1);
+            txtResult.Text = s;
+        }
+    }
+}
+
---
+++ kayttoliittymaohjelmointi/Labra_10_Tehtava_4/kiuas.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra_10_Tehtava_4
+{
+    class kiuas
+    {
+        //  Lämpötilan arvot on rajattava välille 0-120.00 ja kosteuden arvot välille 0-100.0
+        float temp = 0f;
+        float humid = 0f;
+
+        public float Temp { get { return temp; } }
+        public float Humid { get { return humid; } }
+
+        public void SetTemp (float f)
+        {
+            if (f >= 0 && f <=120)
+            {
+                temp = f;
+            }
+        }
+
+        public void SetHumid(float f)
+        {
+            if (f >= 0 && f <= 100)
+            {
+                humid = f;
+            }
+        }
+    }
+}
